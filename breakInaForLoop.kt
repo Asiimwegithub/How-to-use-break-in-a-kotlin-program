94ab66fcfe4676afad3977bbf91b05b4eb44d4ea
Asiimwe Delick <delickasiimwebanton@gmail.com>
2020-04-26 23:59:44 +0300

    Create breakInaForLoop.kt
    This program is supposed to print from 1-10 but it will stop at 5 immediately because we said it breaks after it finds x equalizes 5

---
+++ breakInaForLoop.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+//How we can use break in a for loop as in kotlin programming language
+var x = 1
+for(x in 1..10){
+println(x)
+if(x==5){
+break
+}
+}
+}
+//This programm will print from 1 to 5 instead of printing upto 10.
