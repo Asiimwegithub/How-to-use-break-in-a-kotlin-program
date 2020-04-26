f10bdc1d33255c41285bc76de1334067e6a7cf14
Asiimwe Delick <delickasiimwebanton@gmail.com>
2020-04-26 23:52:15 +0300

    Update continueBreak.kt

--- continueBreak.kt
+++ continueBreak.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
-    //we wanna println from 1-10 but we want before we reach 10 let the loop stop at 5
-    // Now we go to a do while loop
+    //we wanna print from 1-10 but we want before we reach 10 let the loop stop at 5
+    // a do while loop
     var x = 1
     do{
         println(x)
@@ -9,6 +9,6 @@ fun main(args: Array<String>){
         }
         x++
     }while (x<=10)
-    //we run again to see if it stops at 5 instead of continuing up to 10
+    //we run to see if it stops at 5 instead of continuing up to 10
 
 }
